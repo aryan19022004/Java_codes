0851499d3449c5aaaf57069863f0e775a798a64d
Aryan tiwari <at7123029@gmail.com>
2025-05-10 20:51:53 +0530

    second java

---
+++ Second.java
@@ -0,0 +1,9 @@
+public class Second {
+      public static void main(String[] args){
+    System.out.println("this is the first java program in the repo java dsa");
+    int a = 4;
+    int b = 8;
+    int c = a+b;
+    System.out.println(c);
+   } 
+}
