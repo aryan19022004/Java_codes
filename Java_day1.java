de70534578389d9d55b82f43469fb5b4de220eea
Aryan tiwari <at7123029@gmail.com>
2025-06-18 16:20:29 +0530

    MY conditional program in java

---
+++ Java_day1.java
@@ -0,0 +1,11 @@
+public class Java_day1 {
+   public static void main(String[] args){
+       int a = 5;
+       int b = 6;
+       if( a < b ){
+           System.out.println("yes");
+       }else{
+           System.out.println("No");
+       }
+   }
+}
