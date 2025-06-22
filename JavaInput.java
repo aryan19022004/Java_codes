9b2eff0378b2017ad1779e7e3a996517ef814af6
Aryan tiwari <at7123029@gmail.com>
2025-06-22 17:11:03 +0530

    First program to take inputs in the java

---
+++ JavaInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class JavaInput {
+    public  static void main(String[] args){
+        System.out.println("taking input from the user");
+        Scanner s1 = new Scanner(System.in);
+        int a =  s1.nextInt();
+        int b = s1.nextInt();
+        String c = s1.nextLine();
+        System.out.println(a);
+    }
+}
