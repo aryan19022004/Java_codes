a7b52e2a53db3ad1bf9810b5de2e0d2a302bed38
Aryan tiwari <at7123029@gmail.com>
2025-06-22 17:11:11 +0530

    First program to take inputs in the java

--- Java_day1.java
+++ Java_day1.java
@@ -1,20 +1,45 @@
+import java.util.Scanner;
+
 public class Java_day1 {
-   public static void main(String[] args){
-      /* int a = 5;
-       int b = 6;
-       if( a < b ){
-           System.out.println("yes");
-       }else{
-           System.out.println("No");
-       }*/
-       int number = 2; // The number whose table is to be printed
 
-       System.out.println("Multiplication Table of " + number + ":");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check for primality: ");
+        int number = scanner.nextInt();
 
-       // Loop from 1 to 10 to generate the table
-       for (int i = 1; i <= 10; i++) {
-           // Calculate and print each line of the table
-           System.out.println(number + " x " + i + " = " + (number * i));
-       }
-   }
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPrime(int num) {
+        // 0, 1, and negative numbers are not prime.
+        if (num <= 1) {
+            return false;
+        }
+
+        // 2 is the only even prime number.
+        if (num == 2) {
+            return true;
+        }
+
+        // If the number is even and greater than 2, it's not prime.
+        if (num % 2 == 0) {
+            return false;
+        }
+
+        // Check for divisibility from 3 up to the square root of the number,
+        // incrementing by 2 to only check odd divisors.
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0) {
+                return false; // If divisible by any number, it's not prime.
+            }
+        }
+
+        return true; // If no divisors are found, it's prime.
+    }
 }
